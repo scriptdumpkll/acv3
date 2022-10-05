44825ffb3cad0ec2f8c534befb4f942f4d65f1d4
scriptdumpkll <113224535+scriptdumpkll@users.noreply.github.com>
2022-10-05 16:44:44 +0800

    Update stop.lua

--- cmds/stop.lua
+++ cmds/stop.lua
@@ -1,6 +1,7 @@
 if STOP_LOADED == true then return end
 getgenv().STOP_LOADED = true
 function StopCmd()
+    game.ReplicatedStorage.MainEvent:FireServer("Block",true)
     pcall(function()
         game:service"RunService":UnbindFromRenderStep("DROP_CMD")
     end)
